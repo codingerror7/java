88bac286f57a769112987ca423a6ff1a6cc3d426
sujal <saraswatsujal@gmail.com>
2026-03-16 19:40:56 +0530

    inserting node in linked list

--- src/linkedlist/Link.java
+++ src/linkedlist/Link.java
@@ -20,28 +20,121 @@ package src.linkedlist;
 // }
 
 
-public class Link{
-    static class Node{
-        int data;
-        Node next;
+// public class Link{
+//     static class Node{
+//         int data;
+//         Node next;
 
-        Node(int data){       //constructor
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public static void main(String args[]){
-        Node head = new Node(10);
-        Node second = new Node(20);
-        Node third = new Node(30);
-        head.next = second;
-        second.next = third;
+//         Node(int data){       //constructor
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
+//     public static void main(String args[]){
+//         Node head = new Node(10);
+//         Node second = new Node(20);
+//         Node third = new Node(30);
+//         head.next = second;
+//         second.next = third;
 
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
-}
+//         Node temp = head;
+//         while(temp!=null){
+//             System.out.print(temp.data + "->");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
+// }
+
+
+//Insertion at beginning:
+// public class Link{
+//     static class Node{
+//         int data;
+//         Node next;
+
+//         Node(int data){
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
+//     public static void main(String args[]){
+//         Node newhead = new Node(5);
+//         Node head = new Node(10);
+//         Node second = new Node(20);
+//         Node third = new Node(30);
+
+//         head.next = second;
+//         second.next = third;
+
+//         //insertion at beginning
+//         newhead.next = head;
+//         head = newhead;
+
+//         Node temp = head;
+//         while(temp!=null){
+//             System.out.print(temp.data + "->");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
+// }
+
+
+//inserting multiple elements at beginning
+// public class Link{
+//     static class Node{
+//         int data;
+//         Node next;
+
+//         Node(int data){
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
+//     public static void main(String args[]){
+//         Node newNode6 = new Node(1);
+//         Node newNode5 = new Node(3);
+//         Node newNode4 = new Node(4);
+//         Node newNode3 = new Node(5);
+//         Node newNode2 = new Node(8);
+//         Node newNode = new Node(9);
+//         Node head = new Node(10);
+//         Node second = new Node(20);
+//         Node third = new Node(30);
+
+//         head.next = second;
+//         second.next = third;
+         
+//         //inserting first newnode
+//         newNode.next = head;
+//         head = newNode;
+
+//         //inserting second new node
+//         newNode2.next = head;
+//         head = newNode2;
+
+//         //inserting third new node
+//         newNode3.next = head;
+//         head = newNode3;
+
+//         //inserting fourth new node
+//         newNode4.next = head;
+//         head = newNode4;
+
+//         //inserting fifth new node
+//         newNode5.next = head;
+//         head = newNode5;
+
+//         //inserting sixth new node
+//         newNode6.next = head;
+//         head = newNode6;
+
+//         Node temp = head;
+//         while(temp!=null){
+//             System.out.print(temp.data + "->");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
+// }
