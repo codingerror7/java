1c15a2f44378b57308f2ec5ebddb9bc1b6b1ba7d
sujal <saraswatsujal@gmail.com>
2026-03-29 23:45:33 +0530

    streak break nhi honi chiye

--- src/linkedlist/Link.java
+++ src/linkedlist/Link.java
@@ -697,38 +697,3 @@ import java.util.Scanner;
 // }
 
 
-//delete last node
-// public class Link{
-//     static class Node{
-//         int data;
-//         Node next;
-
-//         Node(int data){
-//             this.data = data;
-//             this.next = null;
-//         }
-//     }
-//     public static void main(String args[]){
-//         Node head = new Node(10);
-//         Node second = new Node(20);
-//         Node third = new Node(30);
-
-//         head.next = second;
-//         second.next = third;
-
-//         Node temp = head;
-//         while(temp.next.next!=null){
-//             temp = temp.next;
-//         }
-//         temp.next = null;
-
-//         temp=head;
-//         while(temp!=null){
-//             System.out.print(temp.data + "->");
-//             temp=temp.next;
-//         }
-//         System.out.println("null");
-//     }
-// }
-
-
