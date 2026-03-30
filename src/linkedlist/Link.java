5007c9b65f7a93d371297602c2984806536d1612
sujal <saraswatsujal@gmail.com>
2026-03-30 17:32:35 +0530

    cleaning false code

--- src/linkedlist/Link.java
+++ src/linkedlist/Link.java
@@ -732,30 +732,3 @@ import java.util.Scanner;
 // }
 
 
-public class Link{
-    static class Node{
-        int data;
-        Node next;
-
-        Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public static void main(String args[]){
-        Node head = new Node(10);
-        Node second = new Node(20);
-        Node third = new Node(30);
-        head.next = second;
-        second.next = third;
-        int count = 0;
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.data + "=>");
-            temp=temp.next;
-            count++;
-        }
-        System.out.println("null");
-        System.out.println(count);
-    }
-}
