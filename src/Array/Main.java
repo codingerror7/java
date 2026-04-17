ecc2de6a9818365666acc6cdb244eccd1005e951
sujal <saraswatsujal@gmail.com>
2026-04-17 14:51:16 +0530

    buy and sell stock

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -240,3 +240,27 @@ import java.util.*;
 // }
 
 
+//stock buy sell:
+// public class Main{
+//     public static int buyAndSellStock(int arr[]){
+//         int buyPrice = Integer.MAX_VALUE;
+//         int maxPrice = 0;
+//         for(int i = 0; i < arr.length; i++){
+//             if(buyPrice<arr[i]){
+//                 int profit = arr[i] - buyPrice;
+//                 maxPrice = Math.max(maxPrice, profit);
+//             }
+//             else{
+//                 buyPrice = arr[i];
+//             }
+//         }
+//         return maxPrice;
+//     }
+//     public static void main(String args[]){
+//         int arr[] = {7,1,5,3,6,4};
+//         int res = buyAndSellStock(arr);
+//         System.out.println(res);
+//     }
+// }
+
+
