2a9c6584b115a1d6810bebad568b9a1def516187
sujal <saraswatsujal@gmail.com>
2026-07-26 01:52:41 +0530

    fix

--- src/Patterns/Pattern.java
+++ src/Patterns/Pattern.java
@@ -123,3 +123,23 @@ import java.util.Scanner;
 //         }
 //     }
 // }
+
+
+
+//pyramid:
+// public class Pattern{
+//     public static void main(String agrs[]){
+//         int n = 5;
+//         for(int i = 1; i <= n; i++){
+//             for(int j = 1; j <= n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j <= 2*i-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println(
+
+//             );
+//         }
+//     }
+// }
