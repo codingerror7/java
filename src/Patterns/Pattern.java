754cfc023e0c3060cb0fc87cacf303abdc4ff9b1
sujal <saraswatsujal@gmail.com>
2026-07-26 01:06:15 +0530

    fix

---
+++ src/Patterns/Pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 1; i <= 5; i++){
+//             System.out.println("*");
+//         }
+//     }
+// }
+
+//rectangle:
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 1; i <= 5; i++){         //outer loop for rows
+//             for(int j = 1; j <= 5; j++){     //inner loop for columns
+//                 System.out.print("*");
+//             }
+//             System.out.println();            //to move cursor to another line
+//         }
+//     }
+// }
