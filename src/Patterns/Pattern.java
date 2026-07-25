4b342dfb353b19b6769358462c67f10d95cdc137
sujal <saraswatsujal@gmail.com>
2026-07-26 01:28:12 +0530

    fix

--- src/Patterns/Pattern.java
+++ src/Patterns/Pattern.java
@@ -97,3 +97,16 @@ import java.util.Scanner;
 //         }
 //     }
 // }
+
+
+//inverted right triangle of numbers
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 5; i >= 1; i--){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
