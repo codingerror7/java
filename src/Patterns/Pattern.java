19e2b8ad52f072b27af30b41f6e649637a72fd76
sujal <saraswatsujal@gmail.com>
2026-07-26 01:10:56 +0530

    pattern

--- src/Patterns/Pattern.java
+++ src/Patterns/Pattern.java
@@ -19,3 +19,29 @@ import java.util.Scanner;
 //         }
 //     }
 // }
+
+
+
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 1; i <= 5; i++){
+//             for(int j = 1; j <= 5; j++){
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 1; i <= 5; i++){
+//             for(int j = 1; j <= 5; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
