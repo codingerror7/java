6c16a4a9d7afc99caed89582c311d155290306a9
sujal <saraswatsujal@gmail.com>
2026-07-26 01:57:59 +0530

    fix

--- src/Patterns/Pattern.java
+++ src/Patterns/Pattern.java
@@ -146,17 +146,17 @@ import java.util.Scanner;
 
 
 //inverted pyramid
-public class Pattern{
-    public static void main(String args[]){
-        int n = 5;
-        for(int i = n; i >= 1; i--){
-            for(int j = n-i; j >= 1; j--){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <=2*i-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
+// public class Pattern{
+//     public static void main(String args[]){
+//         int n = 5;
+//         for(int i = n; i >= 1; i--){
+//             for(int j = n-i; j >= 1; j--){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j <=2*i-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
