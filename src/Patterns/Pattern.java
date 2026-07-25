10fa3312d3b8c03813107ce6cff71a24ba3edca7
sujal <saraswatsujal@gmail.com>
2026-07-26 02:18:46 +0530

    fix

--- src/Patterns/Pattern.java
+++ src/Patterns/Pattern.java
@@ -162,6 +162,7 @@ import java.util.Scanner;
 // }
 
 
+
 //upper pyramid reverse pyramid
 // public class Pyramid{
 //     public static void main(String args[]){
@@ -181,6 +182,24 @@ import java.util.Scanner;
 //                 System.out.print(" ");
 //             }
 //             for(int j = 1; j <= 2*i-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class Pattern{
+//     public static void main(String args[]){
+//         for(int i = 1; i <= 5; i++){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for(int i = 4; i >= 1; i--){
+//             for(int j = 1; j <= i; j++){
 //                 System.out.print("*");
 //             }
 //             System.out.println();
