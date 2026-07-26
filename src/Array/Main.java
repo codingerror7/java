7e5f13e284d7ebde1611e3d3dddec3e4289ea9bf
sujal <saraswatsujal@gmail.com>
2026-07-26 16:27:10 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -912,18 +912,3 @@ package src.Array;
 //         }
 // }
 
-
-public class Main{
-    public static void main(String args[]){
-        int arr[] = {1,2,3,4,5};
-        int n = arr.length;
-        int first = arr[0];
-        for(int i = 0; i < n-1; i++){
-            arr[i]=arr[i+1];
-        }
-        arr[n-1]=first;
-        for(int i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
