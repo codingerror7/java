5fa0c02169b17fc5f4fd36fb8f3cc8187bc8ffe7
sujal <saraswatsujal@gmail.com>
2026-07-26 16:09:34 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -913,21 +913,3 @@ package src.Array;
 // }
 
 
-
-public class Main{
-    public static void main(String args[]){
-        int arr[] = {12,2,3,34,45,5,66,778,766,544,44,4};
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-        for(int i = 0; i < arr.length; i++){
-            if(largest<arr[i]){
-                largest=arr[i];
-            }
-            else if(smallest>arr[i]){
-                smallest=arr[i];
-            }
-        }
-        System.out.println("largest is" + " " + largest);
-        System.out.println("smallest is" + " " + smallest);
-    }
-}
