f559a09bc52cf38f7a7b16b43b1541a8c716cd16
sujal <saraswatsujal@gmail.com>
2026-07-26 15:49:47 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -914,3 +914,20 @@ package src.Array;
 
 
 
+public class Main{
+    public static void main(String args[]){
+        int arr[] = {23,34,45,56,67,76,54,43,33,22,21,10};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(largest<arr[i]){
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(secondLargest>arr[i] && largest<arr[i]){
+                secondLargest=arr[i];
+            }
+        }
+        System.out.println(secondLargest);
+    }
+}
