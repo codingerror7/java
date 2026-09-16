39998fd6e6eb10f866e79f454e9d32f7f79a98d7
sujal <saraswatsujal@gmail.com>
2026-09-16 17:31:38 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -912,4 +912,13 @@ package src.Array;
 //         }
 // }
 
-
+public class Main{
+    public static void main(String args[]){
+        for(int i = 0; i < 10; i++){
+            if(i==2){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
