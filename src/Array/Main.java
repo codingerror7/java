6a1b5d89d36533461600a415a65aa03d7f38cf5f
sujal <saraswatsujal@gmail.com>
2026-09-16 17:43:54 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -924,4 +924,23 @@ package src.Array;
 //     }
 // }
 
-
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array size:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter array elements:");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("MAX NO. IS:" + " " + max);
+    }
+}
