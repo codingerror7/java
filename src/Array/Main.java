1cef66061fe5cce78ae098dc18c05e05819135dd
sujal <saraswatsujal@gmail.com>
2026-09-19 00:30:35 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -924,3 +924,15 @@ package src.Array;
 //     }
 // }
 
+public class Main{
+    public static void main(String args[]){
+        int arr[] = new int[4];
+        arr[0] = 3;
+        arr[1] = 4;
+        arr[2] = 4;
+        arr[3] = 6;
+        for(int i = 0; i < 4; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
