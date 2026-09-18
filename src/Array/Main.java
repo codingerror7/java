462eddf849d80b571d39f88e6efabd7da396268b
sujal <saraswatsujal@gmail.com>
2026-09-19 00:38:40 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -924,3 +924,32 @@ package src.Array;
 //     }
 // }
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array size:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("array size is" + " " + n);
+        System.out.println("enter array elements:");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("array elements are:");
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println("enter element to be find in array:");
+        int key = sc.nextInt();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]==key){
+                System.out.println("key found at index" + " " + i);
+                break;
+            }
+            else if(arr[i]!=key && i==arr.length-1){
+                System.out.println("nothing found");
+            }
+        }
+    }
+}
