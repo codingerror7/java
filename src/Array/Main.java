bf91b8a9bcb3c6fd06861c62663145050e488223
sujal <saraswatsujal@gmail.com>
2026-09-20 01:04:34 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -926,3 +926,11 @@ package src.Array;
 
 
 
+public class Main{
+    public static void main(String args[]){
+        String str = "hello";
+        for(int i = 0; i < str.length(); i++){
+            System.out.print(str.charAt(i) + "                  ");
+        }
+    }
+}
