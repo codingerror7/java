08e8b2dfbb4f4f71f63363bf3f50f5e9b8b342e4
sujal <saraswatsujal@gmail.com>
2026-09-20 01:47:35 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -979,7 +979,11 @@ package src.Array;
 import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        
+        int arr[] = {1,2,3,4,5};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
     }
 }
