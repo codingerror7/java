d105001513fc74a942a224585add6d99f3b48e00
sujal <saraswatsujal@gmail.com>
2026-09-19 18:40:38 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -925,35 +925,3 @@ package src.Array;
 // }
 
 
-import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter array size:");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("array size is:" + " " + n);
-        System.out.println("enter array elements:");
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("array elements are:");
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]>largest){
-                largest=arr[i];
-            }
-        }
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]<smallest){
-                smallest=arr[i];
-            }
-        }
-        System.out.println("largest element is" + " " + largest);
-        System.out.println("smallest element is" + " " + smallest);
-    }
-}
