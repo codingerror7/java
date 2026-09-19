5bbc5df01c8c567b4b8e41ccf0824d0e1f428a18
sujal <saraswatsujal@gmail.com>
2026-09-20 00:41:24 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -925,3 +925,17 @@ package src.Array;
 // }
 
 
+public class Main{
+    public static void main(String args[]){
+        int arr[] = {1,2,3,29,0,2,5,6,7,8};
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i]==arr[j+1]){
+                    System.out.println("duplicate element" + " " + i + " " + "is at" + " " + "index" + " " + j);
+                    break;
+                }
+                
+            }
+        }
+    }
+}
