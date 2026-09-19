9df724a505a65018b040441f4dbb258dc40b1edf
sujal <saraswatsujal@gmail.com>
2026-09-20 00:56:59 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -925,3 +925,34 @@ package src.Array;
 // }
 
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array size:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter array elements:");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("array elements are:");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        System.out.println("positive elements are:");
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]>0){
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("negative elements are:");
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]<0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
