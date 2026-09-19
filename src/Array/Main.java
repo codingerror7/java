60fb9a5b234ac423f492e547da09e8e3a7872da9
sujal <saraswatsujal@gmail.com>
2026-09-20 01:31:33 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -975,3 +975,23 @@ package src.Array;
 //         }
 //     }
 // }
+
+
+public class Main{
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        System.out.println("to find second largest:");
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]>largest){
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest=arr[i];
+            }
+        }
+        System.out.println(secondLargest);
+    }
+}
