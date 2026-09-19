865f8a90d3e10f522d2afb872bff869a389e47da
sujal <saraswatsujal@gmail.com>
2026-09-20 01:46:18 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -976,3 +976,10 @@ package src.Array;
 //     }
 // }
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
