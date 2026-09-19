6385c4dd6d1875d2204a7a9f43f8bc6f0181280b
sujal <saraswatsujal@gmail.com>
2026-09-20 01:08:39 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -933,3 +933,16 @@ package src.Array;
 //         }
 //     }
 // }
+
+
+//string reversel
+public class Main{
+    public static void main(String args[]){
+        String str = "Hello";
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev = rev+str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
