45546792ea556e5f459d51c32d74a2a1584b9b77
sujal <saraswatsujal@gmail.com>
2026-09-20 01:09:08 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -936,13 +936,13 @@ package src.Array;
 
 
 //string reversel
-public class Main{
-    public static void main(String args[]){
-        String str = "Hello";
-        String rev = "";
-        for(int i = str.length()-1; i >= 0; i--){
-            rev = rev+str.charAt(i);
-        }
-        System.out.println(rev);
-    }
-}
+// public class Main{
+//     public static void main(String args[]){
+//         String str = "Hello";
+//         String rev = "";
+//         for(int i = str.length()-1; i >= 0; i--){
+//             rev = rev+str.charAt(i);
+//         }
+//         System.out.println(rev);
+//     }
+// }
