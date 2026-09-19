3620c2de1b91dca6e96db49485ac5a72f918969c
sujal <saraswatsujal@gmail.com>
2026-09-20 01:05:00 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -930,7 +930,7 @@ public class Main{
     public static void main(String args[]){
         String str = "hello";
         for(int i = 0; i < str.length(); i++){
-            System.out.print(str.charAt(i) + "                  ");
+            System.out.print(str.charAt(i) + " ");
         }
     }
 }
