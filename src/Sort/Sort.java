38206b0719b771373eea75836ca07c86163d7635
sujal <saraswatsujal@gmail.com>
2026-09-22 18:34:31 +0530

    fix

--- src/Sort/Sort.java
+++ src/Sort/Sort.java
@@ -1,25 +1,26 @@
 package src.Sort;
 
-public class Sort {
-    public static void sortArray(int arr[]){
-        for(int turn = 0; turn < arr.length-1; turn++){
-            for(int j = 0; j < arr.length-1-turn; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-    }
-    public static void printArr(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-    }
-    public static void main(String args[]){
-        int arr[] = {5,78,4,50,78,9,7,5,43};
-        sortArray(arr);
-        printArr(arr);
-    }
-}
+//bubble sort
+// public class Sort {
+//     public static void sortArray(int arr[]){
+//         for(int turn = 0; turn < arr.length-1; turn++){
+//             for(int j = 0; j < arr.length-1-turn; j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//     }
+//     public static void printArr(int arr[]){
+//         for(int i = 0; i < arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+//     public static void main(String args[]){
+//         int arr[] = {5,78,4,50,78,9,7,5,43};
+//         sortArray(arr);
+//         printArr(arr);
+//     }
+// }
