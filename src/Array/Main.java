3e45635224b2cdc6051138acf507057bade20566
sujal <saraswatsujal@gmail.com>
2026-09-22 18:04:36 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -991,35 +991,35 @@ package src.Array;
 //     }
 // }
 
-import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter array size:");
-        int n = sc.nextInt();
-        System.out.println("array size is: " + n);
-        int arr[] = new int[n];
-        System.out.println("enter array elements:");
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("array elements are:");
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
-        }
-        int evenCount = 0; 
-        int oddCount = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]%2==0){
-                evenCount++;
-            }
-        }
-        System.out.println("total even numbers are " + evenCount);
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]%2!=0){
-                oddCount++;
-            }
-        }
-        System.out.println("total odd numbers are " + oddCount);
-    }
-}
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter array size:");
+//         int n = sc.nextInt();
+//         System.out.println("array size is: " + n);
+//         int arr[] = new int[n];
+//         System.out.println("enter array elements:");
+//         for(int i = 0; i < arr.length; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         System.out.println("array elements are:");
+//         for(int i = 0; i < arr.length; i++){
+//             System.out.println(arr[i]);
+//         }
+//         int evenCount = 0; 
+//         int oddCount = 0;
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i]%2==0){
+//                 evenCount++;
+//             }
+//         }
+//         System.out.println("total even numbers are " + evenCount);
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i]%2!=0){
+//                 oddCount++;
+//             }
+//         }
+//         System.out.println("total odd numbers are " + oddCount);
+//     }
+// }
