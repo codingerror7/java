bd3fa1e75a6ffe0932039a1661724842b1ced61a
sujal <saraswatsujal@gmail.com>
2026-09-22 17:58:14 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -977,15 +977,16 @@ package src.Array;
 // }
 
 
-public class Main{
-    public static void main(String args[]){
-        int arr[] = {1,2,3,4,5,6,7,8,9};
-        int count = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]%2!=0){
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-}
+//count odd numbers:
+// public class Main{
+//     public static void main(String args[]){
+//         int arr[] = {1,2,3,4,5,6,7,8,9};
+//         int count = 0;
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i]%2!=0){
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// }
