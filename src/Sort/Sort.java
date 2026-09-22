32201c6622168e00a308bc098d88bb36a87646e0
sujal <saraswatsujal@gmail.com>
2026-09-22 18:50:05 +0530

    fix

--- src/Sort/Sort.java
+++ src/Sort/Sort.java
@@ -28,3 +28,29 @@ package src.Sort;
 
 
 //selection sort
+public class Sort{
+    public static void sortArray(int arr[]){
+        for(int i = 0; i < arr.length-1; i++){
+            int minPos = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[minPos]>arr[j]){
+                    minPos = j;
+                }
+            }
+            //swapping
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {6,5,3,31,22,78,9};
+        sortArray(arr);
+        printArr(arr);
+    }
+}
