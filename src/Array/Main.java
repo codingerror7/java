05b8d3c6be101196b328dae767aaadfbf9aa24ce
sujal <saraswatsujal@gmail.com>
2026-09-23 18:45:31 +0530

    fix

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1035,23 +1035,25 @@ import java.util.Arrays;
 //     }
 // }
 
-import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = 3;
-        int m = 3;
-        int arr[][] = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
+
+//2D arrays:
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = 3;
+//         int m = 3;
+//         int arr[][] = new int[n][m];
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < m; j++){
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < m; j++){
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
